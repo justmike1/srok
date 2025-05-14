e83ed280a1afe0f521ed301c7b24abf4d8dead4f
Mike Joseph <mikej@Mikes-New-MacBook-Pro.local>
2025-05-15 00:04:47 +0800

    add build for srok
    Signed-off-by: Mike Joseph <mikej@Mikes-New-MacBook-Pro.local>

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,34 @@ use leptos::config::get_configuration;
 use leptos_axum::{generate_route_list, handle_server_fns_with_context, LeptosRoutes};
 use log::info;
 use srok::app::{shell, App};
+use std::net::SocketAddr;
+
+async fn healthcheck() -> &'static str {
+    "ok"
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
     env_logger::init();
+
     info!("Starting Srok server...");
 
     let conf = get_configuration(None).unwrap();
     let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr.clone();
+    let addr: SocketAddr = leptos_options.site_addr.parse().unwrap();
 
     let options_for_leptos_routes = leptos_options.clone();
     let options_for_state = leptos_options.clone();
 
     let app = Router::new()
+        .route("/healthz", get(healthcheck))
         .route(
             "/api/*fn_name",
             get(move |req| async move { handle_server_fns_with_context(|| (), req).await }),
         )
         .leptos_routes(
-            &options_for_leptos_routes.clone(),
+            &options_for_leptos_routes,
             generate_route_list(App),
             move || shell(options_for_leptos_routes.clone()),
         )
